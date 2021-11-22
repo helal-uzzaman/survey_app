8058207178d35eaaddfad8b5f2c8f8b34e9b1b70
Helaluzzaman <64187745+Helaluzzaman@users.noreply.github.com>
2021-11-22 11:04:50 +0600

    User login worked.

--- lib/main.dart
+++ lib/main.dart
@@ -1,72 +1,147 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'dart:async';
 
+import 'package:survey_app/authentication.dart';
+
 void main() {
-  runApp(const MyApp());
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => ApplicationState(),
+      builder: (context, _) => MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  void init() async {
-    var result = await Firebase.initializeApp();
-    print(result);
-  }
-
-  Future<UserCredential> signIn(
-    String email,
-    String password,
-  )
-  // void Function(FirebaseAuthException e) errorCallBack)
-  async {
-    var user = await FirebaseAuth.instance
-        .signInWithEmailAndPassword(email: email, password: password);
-
-    return user;
-  }
-  Future<UserCredential> register(
-    String email,
-    String password,
-  )
-  // void Function(FirebaseAuthException e) errorCallBack)
-  async {
-    var user = await FirebaseAuth.instance
-        .createUserWithEmailAndPassword(email: email, password: password);
-
-    return user;
-  }
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "Survey App",
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text("Login Page"),
-        ),
-        body: Center(
-          child: Padding(
-              padding: EdgeInsets.all(16),
-              child: Column(
-                children: [
-                  ElevatedButton(
-                    child: const Text("Initialize firebase"),
-                    onPressed: () => {init()},
-                    autofocus: true,
-                  ),
-                  ElevatedButton(
-                    child: const Text("Log in"),
-                    onPressed: () => {
-                      register("helal@gmail.com", "helal@1234"),
-                    },
-                    autofocus: true,
-                  ),
-                ],
-              )),
-        ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Survey"),
+      ),
+      body: ListView(
+        children: [
+          Consumer<ApplicationState>(
+              builder: (context, appState, _) => Authentication(
+                  loginState: appState._loginState,
+                  email: appState.email,
+                  startLoginFlow: appState.startLoginFlow,
+                  verifyEmail: appState.verifyEmail,
+                  signInWithEmailAndPassword:
+                      appState.signInWithEmailAndPassword,
+                  cancelRegistration: appState.cancelRegistration,
+                  registerAccount: appState.registerAccount,
+                  signOut: appState.signOut))
+        ],
       ),
     );
+  }
+}
+
+// use of provider for statemanagement.
+class ApplicationState extends ChangeNotifier {
+  ApplicationLoginState _loginState = ApplicationLoginState.loggedOut;
+  ApplicationLoginState get loginState => _loginState;
+
+  // user input variables
+  String? _email;
+  String? get email => _email;
+
+  ApplicationState() {
+    myInit();
+  }
+  Future<void> myInit() async {
+    FirebaseApp firebaseInit = await Firebase.initializeApp();
+    print("Firebase init fun call => $firebaseInit");
+
+    FirebaseAuth.instance.userChanges().listen(
+      (user) {
+        if (user != null) {
+          _loginState = ApplicationLoginState.loggedIn;
+          print("User logged in: User = $user");
+        } else {
+          _loginState = ApplicationLoginState.loggedOut;
+          print("User logged out");
+        }
+        notifyListeners();
+      },
+    );
+  }
+
+  void startLoginFlow() {
+    _loginState = ApplicationLoginState.emailAddress;
+    notifyListeners();
+  }
+
+  void verifyEmail(
+    String email,
+    void Function(FirebaseAuthException e) errorCallback,
+  ) async {
+    try {
+      var methods =
+          await FirebaseAuth.instance.fetchSignInMethodsForEmail(email);
+      if (methods.contains("password")) {
+        _loginState = ApplicationLoginState.password;
+      } else {
+        _loginState = ApplicationLoginState.register;
+      }
+      _email = email;
+      notifyListeners();
+    } on FirebaseAuthException catch (e) {
+      errorCallback(e);
+    }
+  }
+
+  void signInWithEmailAndPassword(
+    String email,
+    String password,
+    void Function(FirebaseAuthException e) errorCallback,
+  ) async {
+    try {
+      await FirebaseAuth.instance
+          .signInWithEmailAndPassword(email: email, password: password);
+    } on FirebaseAuthException catch (e) {
+      errorCallback(e);
+    }
+  }
+
+  void cancelRegistration() {
+    _loginState = ApplicationLoginState.emailAddress;
+  }
+
+  void registerAccount(
+    String email,
+    String displayName,
+    String password,
+    void Function(FirebaseAuthException e) errorCallback,
+  ) async {
+    try {
+      UserCredential credential = await FirebaseAuth.instance
+          .createUserWithEmailAndPassword(email: email, password: password);
+      await credential.user!.updateDisplayName(displayName);
+    } on FirebaseAuthException catch (e) {
+      errorCallback(e);
+    }
+  }
+
+  void signOut() {
+    FirebaseAuth.instance.signOut();
   }
 }
