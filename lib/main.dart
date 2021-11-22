25fdf905dec16d1add53f92faadb649913a5580a
Helaluzzaman <64187745+Helaluzzaman@users.noreply.github.com>
2021-11-22 08:32:19 +0600

    firebase initialized and worked for android

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,42 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-
+import 'dart:async';
 
 void main() {
-  WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
+
+  void init() async {
+    var result = await Firebase.initializeApp();
+    print(result);
+  }
+
+  Future<UserCredential> signIn(
+    String email,
+    String password,
+  )
+  // void Function(FirebaseAuthException e) errorCallBack)
+  async {
+    var user = await FirebaseAuth.instance
+        .signInWithEmailAndPassword(email: email, password: password);
+
+    return user;
+  }
+  Future<UserCredential> register(
+    String email,
+    String password,
+  )
+  // void Function(FirebaseAuthException e) errorCallBack)
+  async {
+    var user = await FirebaseAuth.instance
+        .createUserWithEmailAndPassword(email: email, password: password);
+
+    return user;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,13 +48,23 @@ class MyApp extends StatelessWidget {
         ),
         body: Center(
           child: Padding(
-            padding: EdgeInsets.all(16),
-            child: ElevatedButton(
-              child: const Text("Log in"),
-              onPressed: () => {},
-              autofocus: true,
-            ),
-          ),
+              padding: EdgeInsets.all(16),
+              child: Column(
+                children: [
+                  ElevatedButton(
+                    child: const Text("Initialize firebase"),
+                    onPressed: () => {init()},
+                    autofocus: true,
+                  ),
+                  ElevatedButton(
+                    child: const Text("Log in"),
+                    onPressed: () => {
+                      register("helal@gmail.com", "helal@1234"),
+                    },
+                    autofocus: true,
+                  ),
+                ],
+              )),
         ),
       ),
     );
